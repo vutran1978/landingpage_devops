3b83467e4a67261b0664c18cd6170eeefcd41d81
Vu Tran Xuan <txvu20@gmail.com>
2025-07-21 16:26:03 +0700

    Check FE 1

--- frontend/src/__tests__/App.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders landing page title', () => {
-  render(<App />);
-  const heading = screen.getByText(/Về chúng tôi/i);
-  expect(heading).toBeInTheDocument();
-});
